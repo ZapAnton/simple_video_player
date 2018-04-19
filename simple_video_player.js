07517c52317c43480e85adb58bc0dde77b38ff1c
ZapAnton <wc3ft2@gmail.com>
2018-04-19 13:11:22 +0300

    Added function and a button to play/pause video

--- simple_video_player.js
+++ simple_video_player.js
@@ -1,9 +1,31 @@
-var simple_video_player;
+var videoPlayer;
 
-document.addEventListener("DOMContentLoaded", function() {init_media_player();}, false);
+document.addEventListener("DOMContentLoaded", function() {initVideoPlayer();}, false);
 
-function init_media_player() {
-	simple_video_player = document.getElementById('media_video');
+function initVideoPlayer() {
+	videoPlayer = document.getElementById('media-video');
 
-	simple_video_player.controls = false;
+	videoPlayer.controls = false;
+}
+
+function setButtonType(button, value) {
+	button.title = value;
+
+	button.innerHTML = value;
+
+	button.className = value;
+}
+
+function togglePlayPause() {
+	var button = document.getElementById('play-pause-button');
+
+	if (videoPlayer.paused || videoPlayer.ended) {
+		setButtonType(button, 'pause')
+
+		videoPlayer.play();
+	} else {
+		setButtonType(button, 'play')
+
+		videoPlayer.pause();
+	}
 }
